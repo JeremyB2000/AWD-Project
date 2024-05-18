0852b8594f272e33942403818c687ecdaeaa8d34
JeremyB2000 <101437554+JeremyB2000@users.noreply.github.com>
2024-05-18 22:07:13 +0800

    Mistake. Fixed

--- apps/form.py
+++ apps/form.py
@@ -8,3 +8,18 @@ class LoginForm(wtforms.Form):
     password = wtforms.PasswordField('password', validators=[DataRequired()])
 
 
+class RegistrationForm(wtforms.Form):
+    username = wtforms.StringField('username', validators=[DataRequired()])
+    email = wtforms.StringField('email', validators=[Email()])
+    password = wtforms.PasswordField('password', validators=[DataRequired()])
+    confirmPassword = wtforms.PasswordField(validators=[EqualTo('password')])
+
+    def validate_username(self, username):
+        user = AccountDimension.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = AccountDimension.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already in use. Please choose a different one or login.')
