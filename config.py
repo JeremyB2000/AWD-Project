3a9c81be06e02759db1db673faef2e1348c4aa74
JeremyB2000 <101437554+JeremyB2000@users.noreply.github.com>
2024-05-18 20:05:03 +0800

    Updated secret key to random 24 byte number

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'apps.db')
-    SECRET_KEY = 'ASDWADSDFAGDGFJ'
+    SECRET_KEY = os.urandom(24) #urandom generates a 24 byte secret key
