e63b002acdbe76006e7381707217227178a142fe
Zackggiitt <161786105+Zackggiitt@users.noreply.github.com>
2024-05-19 14:08:06 +0800

    Delete static directory

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('request-btn').addEventListener('click', function() {
-    document.getElementById('popupForm').style.display = 'block';
-});
-
-document.getElementById('closeFormButton').addEventListener('click', function() {
-    document.getElementById('popupForm').style.display = 'none';
-});
