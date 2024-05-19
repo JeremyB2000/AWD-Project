0f47213999d78852a600c44243d52141b8edd7b2
Zackggiitt <161786105+Zackggiitt@users.noreply.github.com>
2024-05-19 14:09:43 +0800

    stylesheet

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('request-btn').addEventListener('click', function() {
+    document.getElementById('popupForm').style.display = 'block';
+});
+
+document.getElementById('closeFormButton').addEventListener('click', function() {
+    document.getElementById('popupForm').style.display = 'none';
+});
